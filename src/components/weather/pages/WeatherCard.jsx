39cd7d25faed245a815f9efb74edd77c5516748f
SPsagar404 <sagarpujari404@gmail.com>
2023-06-15 18:41:06 +0530

    final

--- src/components/weather/pages/WeatherCard.jsx
+++ src/components/weather/pages/WeatherCard.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './WeatherCard.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCloud,faDroplet} from '@fortawesome/free-solid-svg-icons';
+import { faCloud,faDroplet,faTemperatureThreeQuarters} from '@fortawesome/free-solid-svg-icons';
 
 
 export const WeatherCard = ({data}) => {
@@ -10,27 +10,46 @@ export const WeatherCard = ({data}) => {
     return (
         <>
 
-            <div className=" mt-5  " >
-                <div className="row g-0">
-                    <div className="col-md-6 align-content-center border-1 ">
-                        <h5 className="city-country">{city},{country}</h5>
-                        <img src={icon} className="img-fluid rounded-start mb-2 mt-2 text-center" alt="..."/>
-                        <h5 className="temperature">{temperature}°C</h5>
-                    </div>
-                    <div className="col-md-6">
-                        <div className="card-body">
+            <div className=' mx-auto'>
 
-                            <div className="card-text text-center description mb-5">
-                                <FontAwesomeIcon icon={faCloud} className="icon "/>
-                                <h5>{description}</h5></div>
+                    <div className="row">
+                        <div className='col-md-6'>
+                            <div className="row mt-5">
+                                <h5 className="text-center">{city},{country}</h5>
+
+                            </div>
+                            <div className="row">
+                                <img src={icon} className="img-fluid rounded-start mx-auto weather-img" alt="weather condtion"  />
+
+                            </div>
+                        </div>
+                        <div className='col-md-6'>
+                            <div className="row">
+                                <p className="text-center">
+                                    <FontAwesomeIcon icon={faTemperatureThreeQuarters} className="mx-auto icon"/>
+                                </p>
+                                <h5 className="text-center">{temperature}°C</h5>
+
+                            </div>
+                            <div className="row">
+                                <p className="text-center">
+                                    <FontAwesomeIcon icon={faCloud} className="mx-auto icon"/>
+                                </p>
+                                <h5 className='text-center'>{description}</h5>
+
+                            </div>
+                            <div className="row mt-4">
+                                <p className="text-center">
+                                    <FontAwesomeIcon icon={faDroplet} className="icon"/>
+                                </p>
+                                <h5 className="text-center">{humidity}%</h5>
+
+                            </div>
 
                         </div>
-                        <div className="card-text text-center description mt-5">
-                            <FontAwesomeIcon icon={faDroplet} className="icon "/>
-                            <h5>{humidity}%</h5>
-                        </div>
                     </div>
-                </div>
+
+
             </div>
 
         </>
